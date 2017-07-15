faeeb5663a4f7d41a359d5cbfbe0e65d2f811376
Jonathan Kamens <jik@quantopian.com>
2017-07-14 22:32:18 -0400

    Log output of failed plugins

--- client/collect.py
+++ client/collect.py
@@ -65,6 +65,7 @@ def run_dir(dir_name, parse_output=True, delete_after_success=False,
                     stderr=stderr_file.fileno())
             except subprocess.CalledProcessError as e:
                 log.exception('Failed to execute {}', run_path)
+                log.debug('Output of failed script:\n{}', e.output)
                 if submit_failures:
                     stderr_file.seek(0)
                     results[run_name] = {
