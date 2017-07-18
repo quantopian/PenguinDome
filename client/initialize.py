14493dd7f46a0a40219cff46a769dd414fcf8c59
Joe Jevnik <joe@quantopian.com>
2017-07-18 16:11:37 -0400

    Add arch linux pkgbuilds so the client can be installed with pacman

--- client/initialize.py
+++ client/initialize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 from tempfile import NamedTemporaryFile
 from textwrap import dedent
@@ -8,17 +9,41 @@ import shutil
 from qlmdm import top_dir
 from qlmdm.prompts import get_bool
 
+
 os.chdir(top_dir)
 
-cron_file = '/etc/cron.d/qlmdm'
+
+parser = argparse.ArgumentParser(
+    description='Finalize installation of the qlmdm client.',
+)
+parser.add_argument(
+    '--prefix',
+    metavar='PATH',
+    default='/',
+    help='The install prefix.',
+)
+parser.add_argument(
+    '-y',
+    '--no-prompt',
+    action='store_false',
+    dest='prompt',
+    help='Do not prompt the user before installing or replacing the crontab',
+)
+
+args = parser.parse_args()
+
+cron_file = os.path.join(args.prefix, 'etc/cron.d/qlmdm')
 cron_exists = os.path.exists(cron_file)
 
-if cron_exists:
-    prompt = 'Do you want to replace the crontab?'
-else:
-    prompt = 'Do you want to install the crontab?'
+if prompt:
+    if cron_exists:
+        prompt = 'Do you want to replace the crontab?'
+    else:
+        prompt = 'Do you want to install the crontab?'
 
-do_crontab = get_bool(prompt, not cron_exists)
+    do_crontab = get_bool(prompt, not cron_exists)
+else:
+    do_crontab = True
 
 if do_crontab:
     with NamedTemporaryFile('w+') as temp_cron_file:
