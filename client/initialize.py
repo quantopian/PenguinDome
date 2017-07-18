33557734da3c74fbc55e826c454f1e0f0bf3a56a
Joe Jevnik <joe@quantopian.com>
2017-07-18 18:16:09 -0400

    args.prefix, not prefix

--- client/initialize.py
+++ client/initialize.py
@@ -50,7 +50,9 @@ if do_crontab:
         temp_cron_file.write(dedent('''\
             SHELL=/bin/bash
             * * * * * root {} &>/dev/null
-        '''.format(os.path.join(prefix, 'opt/qlmdm/bin/client/client-cron'))))
+        '''.format(
+            os.path.join(args.prefix, 'opt/qlmdm/bin/client/client-cron'))),
+        )
         temp_cron_file.flush()
         os.chmod(temp_cron_file.name, 0o644)
 
