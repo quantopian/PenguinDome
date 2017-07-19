68a3daf1e0ea33e857c56e8d85f22112584ea8f9
Jonathan Kamens <jik@quantopian.com>
2017-07-19 16:49:30 -0400

    Firewall plugin shouldn't crash if IPv6 is disabled in kernel

--- client/plugins/firewall.py
+++ client/plugins/firewall.py
@@ -8,7 +8,14 @@ chain_re = re.compile(r'^Chain (.*) \(policy (.*)\)')
 
 
 def get_policies(cmd):
-    output = subprocess.check_output((cmd, '-L', '-n')).decode('ascii')
+    try:
+        output = subprocess.check_output(
+            (cmd, '-L', '-n'), stderr=subprocess.STDOUT).decode('ascii')
+    except subprocess.CalledProcessError as e:
+        if re.search(r'Module \S+ not found', e.output.decode('ascii')):
+            # Not enabled in kernel
+            return None
+        print(e.output.decode('ascii'))
     lines = output.strip().split('\n')
     chains = filter(None, (chain_re.match(l) for l in lines))
     return {c.group(1): c.group(2)
@@ -19,12 +26,15 @@ results = {}
 
 results['layers'] = {}
 for layer in ('iptables', 'ip6tables'):
-    results['layers'][layer] = {}
     policies = get_policies(layer)
-    results['layers'][layer]['policies'] = policies
-    results['layers'][layer]['status'] = \
-        'on' if 'ACCEPT' not in (policies['INPUT'], policies['FORWARD']) \
-        else 'off'
+    if policies is None:
+        continue
+    results['layers'][layer] = {
+        'policies': policies,
+        'status': ('on'
+                   if 'ACCEPT' not in (policies['INPUT'], policies['FORWARD'])
+                   else 'off'),
+    }
 
 results['status'] = 'off' if any(l for l in results['layers'].values()
                                  if l['status'] == 'off') else 'on'
