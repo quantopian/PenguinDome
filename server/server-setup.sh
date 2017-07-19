702f3950b61ab6494738341fe74c2e1b71f4b0bf
Jonathan Kamens <jkamens@quantopian.com>
2017-07-19 12:35:30 -0400

    Add support for installing server on arch

--- server/server-setup.sh
+++ server/server-setup.sh
@@ -7,12 +7,61 @@ venv=var/server-venv
 
 cd "$(dirname $0)/.."
 
-. /etc/lsb-release
-if [ "$DISTRIB_ID" = "Ubuntu" ]; then
-    apt-get -qq install $(sed 's/#.*//' server/ubuntu-packages.txt)
-fi
+. /etc/os-release
+
+# For the time being, this script needs to be run as root because
+# initialize.py installs files that only root can install, but
+# eventually we may want to fix that, so I've attempted to write this
+# build script so that it can run as root or non-root.
+arch_build() {
+    git_url="$1"; shift
+    mkdir -p var/makepkg
+    dir=${git_url##*/}
+    dir=${dir%.git}
+    if [ $(id -u) == 0 ]; then
+	TOROOT=
+	FROMROOT="sudo -u nobody"
+	FIXPERMS="chown -R nobody"
+    else
+	TOROOT=sudo
+	FROMROOT=
+	FIXPERMS=
+    fi
+    if ! (
+	    cd var/makepkg
+	    if [ ! -f $dir/*.tar.xz ]; then
+		$TOROOT pacman -S --noconfirm --needed git
+		rm -rf $dir
+		git clone $git_url $dir
+		cd $dir
+		$FIXPERMS .
+		$FROMROOT makepkg -Acs
+		cd ..
+	    fi
+	    $TOROOT pacman -U --needed --noconfirm $dir/*.tar.xz
+      ) > var/makepkg/$dir.log 2>&1; then
+	echo "Installing $git_url failed. See var/makepkg/$dir.log" 1>&2
+	false
+    fi
+    true
+}
 
 mkdir -p var
+
+if [ "$ID_LIKE" = "debian" ]; then
+    # Ubuntu setup will probably work on Debian, though not tested.
+    apt-get -qq install $(sed 's/#.*//' server/ubuntu-packages.txt)
+elif [ "$ID_LIKE" = "archlinux" ]; then
+    if ! pacman -S --needed --noconfirm $(sed -e 's/#.*//' -e '/\.git$/d' \
+      server/arch-packages.txt) >| var/pacman.log 2>&1; then
+	echo "pacman -S failed. See var/pacman.log" 1>&2
+	exit 1
+    fi
+    while read git_url; do
+	arch_build $git_url
+    done < <(sed -n -e 's/#.*//' -e '/\.git$/p' server/arch-packages.txt)
+fi
+
 if [ ! -d $venv ]; then
     virtualenv -p python3 $venv
 fi
