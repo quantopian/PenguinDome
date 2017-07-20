201d60e561854e38987e54773514b05cca5c8774
Jonathan Kamens <jkamens@quantopian.com>
2017-07-20 10:11:59 +0000

    Create server/venv wrapper script on setup
    Creaet a generic script called server/venv which initializes the
    server virtualenv and then invokes its arguments as a command. This
    way we don't have to create wrappers for everything.

--- server/server-setup.sh
+++ server/server-setup.sh
@@ -75,20 +75,30 @@ done
 pip install -q --upgrade pip
 pip install -q -r server/requirements.txt -c server/constraints.txt
 
+make_wrapper() {
+    target="$1"; shift
+    cmd="$1"; shift
+
+    rm -f "$target"
+    cat >>"$target" <<EOF
+#!/bin/bash -e
+. "$(pwd)/$venv/bin/activate"
+exec $cmd "\$@"
+EOF
+    chmod +x "$target"
+}
+
 mkdir -p bin
 for file in server/*.py; do
-    tail=${file##*/}
+    tail="${file##*/}"
     if [ $tail = "initialize.py" ]; then
         continue
     fi
-    target=bin/${tail%.py}
-    rm -f $target
-    cat >>$target <<EOF
-#!/bin/bash -e
-. $(pwd)/$venv/bin/activate
-exec python $(pwd)/$file "\$@"
-EOF
-    chmod +x $target
+    target="bin/${tail%.py}"
+    make_wrapper "$target" "python \"$(pwd)/$file\""
 done
+
+# Generic virtualenv wrapper script
+make_wrapper "server/venv" ""
 
 exec python server/initialize.py "$@"
