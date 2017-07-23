42e44ebbaef1d756bf5a28975a3b1f805a55d982
Jonathan Kamens <jik@quantopian.com>
2017-07-22 20:37:39 -0400

    screenlock and get_session plugins: use cached data when no one is logged in

--- server/issues.py
+++ server/issues.py
@@ -74,7 +74,7 @@ problem_checks = {
         'spec': {'plugins.firewall.status': {'$not': {'$eq': 'on'}}}},
     'screenlock-disabled': {
         'grace-period': datetime.timedelta(hours=4),
-        'spec': {'plugins.screenlock.enabled': {'$not': {'$eq': True}}}},
+        'spec': {'plugins.screenlock.users.enabled': {'$not': {'$eq': True}}}},
     'deprecated-port': {
         'grace-period': datetime.timedelta(hours=1)},
     'pending-patches': {
