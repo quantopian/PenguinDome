9b36b25665ecf7dffa677f675ec7126942512624
Jonathan Kamens <jik@quantopian.com>
2017-07-26 15:16:43 -0400

    Add Apache License

--- client/plugins/sshd.py
+++ client/plugins/sshd.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 
+# Quantopian, Inc. licenses this file to you under the Apache License, Version
+# 2.0 (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
 # Try to identify a running sshd process in two ways: (1) look for a process
 # with the name "sshd" or "in.sshd"; (2) look for a process listening on port
 # 22. If neither of those is found, then assume that sshd is stopped.
