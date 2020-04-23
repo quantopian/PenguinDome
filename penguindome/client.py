quantopian/PenguinDome

commit ae1a3d2cf2127bec5fc4434640ba7f4ad6bab776

Specify GnuPG user ID explicitly so it won't try to use a smartcard
GnuPG commands on clients will fail if there's a smartcard plugged in
with a GnuPG private key in it. Prevent this by specifying the
username of the key to use explicitly so PenguinDome won't try to use
the key on the smartcard.

penguindome/client.py
@@ -31,7 +31,9 @@ from penguindome import (
     top_dir,
 )
 
-gpg_command = partial(main_gpg_command, minimum_version=client_gpg_version)
+gpg_user_id = 'penguindome-client'
+gpg_command = partial(main_gpg_command, '-u', gpg_user_id,
+                      minimum_version=client_gpg_version)
 session = None
 
 

penguindome/server.py
@@ -13,6 +13,7 @@
 from base64 import b64encode
 from collections import defaultdict
 import datetime
+from functools import partial
 from mongo_proxy import MongoProxy
 import os
 from pymongo import MongoClient
@@ -27,9 +28,12 @@ from penguindome import (
     signatures_dir,
     get_selectors as main_get_selectors,
     encrypt_document as main_encrypt_document,
-    gpg_command,
+    gpg_command as main_gpg_command,
     top_dir,
 )
+
+gpg_user_id = 'penguindome-server'
+gpg_command = partial(main_gpg_command, '-u', gpg_user_id)
 
 valid_client_parameters = (
     # This is a list of other client which should be treated as the same as the

server/initialize.py
@@ -36,11 +36,13 @@ from penguindome.client import (
     get_setting as get_client_setting,
     set_setting as set_client_setting,
     save_settings as save_client_settings,
+    gpg_user_id as client_user_id,
 )
 from penguindome.server import (
     get_setting as get_server_setting,
     set_setting as set_server_setting,
     save_settings as save_server_settings,
+    gpg_user_id as server_user_id,
 )
 from penguindome.prompts import (
     get_bool,
@@ -53,9 +55,6 @@ os.chdir(top_dir)
 
 os.makedirs(gpg_private_home, 0o700, exist_ok=True)
 os.makedirs(gpg_public_home, 0o700, exist_ok=True)
-
-server_user_id = 'penguindome-server'
-client_user_id = 'penguindome-client'
 
 entropy_warned = False
 get_string_none = partial(get_string, none_ok=True)

commit 4efd8c6cb758a78857a5dbd5817b78eef79e7f32

Fix client shell, broken in ae1a3d2
The method that I used to specify the GPG user ID explicitly in commit
ae1a3d2 was incorrect because it assumed that all API invocations came
from the client, when in fact the server makes an API invocation when
a user on the server initiates a client shell. This commit fixes the
method so that so the correct GPG user ID Is used when the API is
invoked on the server.

Fixes quantopian/PenguinDome#19.

penguindome/client.py
@@ -31,8 +31,7 @@ from penguindome import (
     top_dir,
 )
 
-gpg_user_id = 'penguindome-client'
-gpg_command = partial(main_gpg_command, '-u', gpg_user_id,
+gpg_command = partial(main_gpg_command, with_user_id=True,
                       minimum_version=client_gpg_version)
 session = None
 

penguindome/penguindome.py
@@ -52,6 +52,10 @@ got_logger = None
 client_gpg_version = '2.1.11'
 server_pipe_log_filter_re = re.compile(
     r'POST.*/server_pipe/.*/(?:send|receive).* 200 ')
+gpg_user_ids = {
+    'client': 'penguindome-client',
+    'server': 'penguindome-server',
+}
 
 SelectorVariants = namedtuple(
     'SelectorVariants', ['plain_mongo', 'plain_mem', 'enc_mongo', 'enc_mem'])
@@ -108,7 +112,7 @@ def set_gpg(mode):
     gpg_mode = mode
 
 
-def gpg_command(*cmd, with_trustdb=False, quiet=True,
+def gpg_command(*cmd, with_user_id=False, with_trustdb=False, quiet=True,
                 minimum_version='2.1.15', log=None):
     global gpg_exe, gpg_exe
 
@@ -141,13 +145,18 @@ def gpg_command(*cmd, with_trustdb=False, quiet=True,
     else:
         trustdb_args = ('--trust-model', 'always')
 
+    if with_user_id:
+        user_id_args = ('-u', gpg_user_ids[gpg_mode])
+    else:
+        user_id_args = ()
+
     if quiet:
         quiet_args = ('--quiet',)
     else:
         quiet_args = ()
 
     cmd = tuple(chain((gpg_exe, '--batch', '--yes'), quiet_args, trustdb_args,
-                      cmd))
+                      user_id_args, cmd))
     try:
         return subprocess.check_output(cmd, stderr=subprocess.STDOUT).\
             decode('utf8')

penguindome/server.py
@@ -32,8 +32,7 @@ from penguindome import (
     top_dir,
 )
 
-gpg_user_id = 'penguindome-server'
-gpg_command = partial(main_gpg_command, '-u', gpg_user_id)
+gpg_command = partial(main_gpg_command, with_user_id=True)
 
 valid_client_parameters = (
     # This is a list of other client which should be treated as the same as the

server/initialize.py
@@ -28,6 +28,7 @@ from penguindome import (
     top_dir,
     gpg_private_home,
     gpg_public_home,
+    gpg_user_ids,
     set_gpg,
     gpg_command,
     releases_dir,
@@ -36,13 +37,11 @@ from penguindome.client import (
     get_setting as get_client_setting,
     set_setting as set_client_setting,
     save_settings as save_client_settings,
-    gpg_user_id as client_user_id,
 )
 from penguindome.server import (
     get_setting as get_server_setting,
     set_setting as set_server_setting,
     save_settings as save_server_settings,
-    gpg_user_id as server_user_id,
 )
 from penguindome.prompts import (
     get_bool,
@@ -199,10 +198,10 @@ def main(args):
     else:
         maybe_changed = maybe_changed_extended
 
-    generate_key('server', server_user_id)
-    generate_key('client', client_user_id)
-    import_key('server', client_user_id)
-    import_key('client', server_user_id)
+    generate_key('server', gpg_user_ids['server'])
+    generate_key('client', gpg_user_ids['client'])
+    import_key('server', gpg_user_ids['client'])
+    import_key('client', gpg_user_ids['server'])
 
     default = not (get_client_setting('loaded') and
                    get_server_setting('loaded'))